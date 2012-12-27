f909649300704b60f5c26c12eddf6cf28841f82a
Otto <Otto@Ottoputer>
2012-12-27 23:50:24 +0100

    Hej

---
+++ GUI/src/GUI.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI extends JFrame {
+ 
+ private JTextArea output;
+ private JTextField input;
+ private JTextArea logUsers;
+ private JButton send;
+ 
+ public GUI(){
+  setTitle("FML IRC");
+  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  setLocationRelativeTo(null);
+  setSize(400, 400);
+  setLayout(new GridLayout(1,2));
+  
+  
+ }
+
+ 
+ public static void main(String[] args) {
+  
+  GUI gui = new GUI();
+  gui.setVisible(true);
+  
+
+ }
+
+}
