0e27338a46e2df3f1642015b9579935f83473bcb
Otto <Otto@Ottoputer>
2012-12-28 00:17:23 +0100

    asd

--- GUI/GUI.java
+++ GUI/GUI.java
@@ -1,32 +1,33 @@
 package grupparbete.chattclient;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class GUI extends JFrame {
+	public class GUI extends JFrame {
  
- private JTextArea output;
- private JTextField input;
- private JTextArea logUsers;
- private JButton send;
+	 private JTextArea output;
+	 private JTextField input;
+	 private JTextArea logUsers;
+	 private JButton send;
  
- public GUI(){
-  setTitle("FML IRC");
-  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  setLocationRelativeTo(null);
-  setSize(400, 400);
-  setLayout(new GridLayout(1,2));
-  
-  
-  
+	 public GUI(){
+	  setTitle("FML IRC");
+	  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  setLocationRelativeTo(null);
+	  setSize(400, 400);
+	  setLayout(new GridLayout(1,2));
+	  
+	  
   }
 
  
- public static void main(String[] args) {
-  
-  GUI gui = new GUI();
-  gui.setVisible(true);
-  
- }
-
-
+	 public static void main(String[] args) {
+	  
+	  GUI gui = new GUI();
+	  gui.setVisible(true);
+	  
+	 
+	 }
+	
+	
 }
